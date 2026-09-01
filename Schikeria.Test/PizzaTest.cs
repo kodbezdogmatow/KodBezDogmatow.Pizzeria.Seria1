b6f1ee38cd619d4066d4a1849f64cfa6dd1dcd40
Kod Bez Dogmatow <kodbezdogmatow@gmail.com>
2026-09-01 14:42:11 +0200

    Odcinek 14 s2o1 - wyliczanie ceny pizzy + test jednostkowy

---
+++ Schikeria.Test/PizzaTest.cs
@@ -0,0 +1,37 @@
+﻿using Schikeria.Model.Pizzas;
+using Schikeria.Model.Toppings;
+
+namespace Schikeria.Test
+{
+    public class PizzaTest
+    {
+        [Fact]
+        public void Test_Price()
+        {
+            // Arrange
+            var pizza = new Pizza
+            {
+                Name = "Test",
+                CurrentSize = Sizes.Large,
+                Toppings = [
+                    new Topping {Name = "T1", Price = 0.5m},
+                    new Topping {Name = "T1", Price = 1m},
+                    new Topping {Name = "T1", Price = 1.5m}
+                    ]
+            };
+
+            pizza.Sizes.Add(Sizes.Small, 10);
+            pizza.Sizes.Add(Sizes.Medium, 20);
+            pizza.Sizes.Add(Sizes.Large, 30);
+
+            // Act
+            var price = pizza.Price;
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.Equal(33, price);
+            });
+        }
+    }
+}
--- Schikeria/Model/Pizzas/Pizza.cs
+++ Schikeria/Model/Pizzas/Pizza.cs
@@ -8,5 +8,15 @@ namespace Schikeria.Model.Pizzas
         public Dictionary<Sizes, decimal> Sizes { get; set; } = [];
         public required string Name { get; set; }
         public List<Topping> Toppings { get; set; } = [];
+
+        public Sizes CurrentSize { get; set; }
+        public decimal Price
+        {
+            get
+            {
+                return Sizes[CurrentSize] +
+                    Toppings.Sum(t => t.Price);
+            }
+        }
     }
 }
