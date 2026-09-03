2e306e19e2c67a50d919d91fbc4f627fe39ed63a
Kod Bez Dogmatow <kodbezdogmatow@gmail.com>
2026-09-03 20:03:38 +0200

    Odcinek 17 s2o4 - Łączymy rabaty i komplikujemy logikę

---
+++ Schikeria/Constants/Discounts/Names.cs
@@ -0,0 +1,13 @@
+﻿namespace Schikeria.Constants.Discounts
+{
+    public static class Names
+    {
+        public const string Price100zl = "Powyżej 100 zł";
+        public const string Student = "Dla Studentów";
+        public const string Monday = "W Poniedziałek";
+        public const string VIP = "VIP";
+        public const string Group2 = "Grupowy 2";
+        public const string Group3 = "Grupowy 3";
+        public const string Group5 = "Grupowy 5";
+    }
+}
