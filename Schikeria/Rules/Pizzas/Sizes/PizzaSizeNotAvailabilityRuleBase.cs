56ab90e7f6f4b75f81a661325656d3916074622e
Kod Bez Dogmatow <kodbezdogmatow@gmail.com>
2026-09-11 16:44:38 +0200

    Odcinek 29: s3o7 regula biznesowa dla dostepnosci pizzy w danym rozmiarze

---
+++ Schikeria/Rules/Pizzas/Sizes/PizzaSizeNotAvailabilityRuleBase.cs
@@ -0,0 +1,18 @@
+﻿using Schikeria.Model.Pizzas;
+
+namespace Schikeria.Rules.Pizzas.Sizes
+{
+    public abstract class PizzaSizeNotAvailabilityRuleBase
+    {
+        public bool IsSatisfied(
+            Pizza pizza, 
+            Model.Pizzas.Sizes sizes)
+        {
+            return SpecifyIsSatisfied(pizza, sizes);
+        }
+
+        protected abstract bool SpecifyIsSatisfied(
+            Pizza pizza, 
+            Model.Pizzas.Sizes sizes);
+    }
+}
